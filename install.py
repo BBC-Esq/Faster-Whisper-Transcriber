2908458a39808f14b812b3bf0657b3ac6eee9332
Chintella-Esq <bbc@chintellalaw.com>
2026-01-15 10:17:44 -0500

    Add files via upload

--- install.py
+++ install.py
@@ -7,9 +7,8 @@ from tkinter import messagebox
 
 
 full_install_libs = [
-    "PySide6==6.9.2",
+    "PySide6==6.9.2"
 ]
-
 
 priority_libs = {
     "cp311": {
@@ -77,12 +76,10 @@ libs = [
     "psutil==7.1.3",
     "pycparser==2.23",
     # "pyinstaller==6.11.1",
-    "pynput==1.8.1",
     "pyreadline3==3.5.4",
     "PyYAML==6.0.3",
     "regex==2025.10.23",
     "requests==2.32.5",
-    "six==1.17.0", # only required by pynput
     "sounddevice==0.5.3",
     "sympy==1.13.3",
     "tokenizers==0.22.1",
@@ -242,5 +239,4 @@ def main():
     print(f"\033[92m\nTotal installation time: {int(hours):02d}:{int(minutes):02d}:{seconds:05.2f}\033[0m")
 
 if __name__ == "__main__":
-
     main()
